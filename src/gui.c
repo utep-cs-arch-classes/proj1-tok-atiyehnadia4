dc41282e3878e6400b9adcc9b86d158dd0d6b159
Nadia Atiyeh <natiyeh@miners.utep.edu>
2021-02-08 09:18:28 -0700

    added gui.c file

---
+++ src/gui.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+  char count;
+char b;
+int char[100];
+int i;
+
+printf("Input a string");
+count = 0;
+i = 0;
+while(count != '\n'){
+  count = getchar();
+  char[i] = count
+    i = i+1;
+ }
+printf("%c " ,char[0])
