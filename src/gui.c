596835edc16450168dbb3141122212a836507e3c
Nadia Atiyeh <natiyeh@miners.utep.edu>
2021-02-08 09:24:39 -0700

    fixed more formatting issues on gui.c and deleted unused variables

--- src/gui.c
+++ src/gui.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 void main()
-char count;
-char b;
-int char[100];
-int i;
+  char count;
+  int char[100];
+  int i;
 
-printf("Input a string");
-count = 0;
-i = 0;
-while(count != '\n'){
-  count = getchar();
-  char[i] = count
+  printf("Input a string");
+  count = 0;
+  i = 0;
+  while(count != '\n'){
+    count = getchar();
+    char[i] = count
     i = i+1;
- }
-printf("%c " ,char[0])
+  }
+  printf("%c " ,char[0])
