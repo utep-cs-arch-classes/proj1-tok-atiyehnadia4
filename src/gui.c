94c3f830744db9f110101e80b00a18acf08f2213
Nadia Atiyeh <natiyeh@miners.utep.edu>
2021-02-14 22:53:30 -0700

    Removed code that was in the wrong file along with it's test cases

--- src/gui.c
+++ src/gui.c
@@ -13,31 +13,7 @@ void userInputGUI(){
   printf("%s\n", userInput);
 }
 
-int space_char(char c){
-  int count;
-  count = 0;
-  if(c < 21){
-    count = count + 1;
-  }
-  return count;
-}
-
-int non_space_char(char c){
-  int count;
-  count = 0;
-  if(c > 20){
-    count = count + 1;
-  }
-  return count;
-}
-
 
 void main(){
   userInputGUI();
-  char name[100] = "Nadia ";
-  int i;
-  for(i = 0; i < 7; i = i+1){
-    printf("%i\n", space_char(name[i]));
-    printf("%i\n", non_space_char(name[i]));
-  }
 }
