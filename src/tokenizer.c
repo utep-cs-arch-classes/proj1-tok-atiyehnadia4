ab03e524c83ea266196eeb46b4152df36c1b421a
Nadia Atiyeh <natiyeh@miners.utep.edu>
2021-02-18 23:39:57 -0700

    added logic to both word_start and word_end, modified space_char and created place holder for word_count

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,20 +1,38 @@
-#include <tokenizer.h>
+#include "tokenizer.h"
 #include <stdio.h>
 
+
 int space_char(char c){
-  int count;
-  count = 0;
-  if(c < 21){
-    count = count + 1;
+  if(c == '\n' || c == '\t' || c == ' '){
+    return 1;
   }
-  return count;
+  return 0;
 }
 
 int non_space_char(char c){
-  int count;
-  count = 0;
-  if(c > 20){
-    count = count + 1;
+  return !space_char(c);
+}
+
+
+char *word_start(char *str){
+  char *c = str;
+  while(space_char(*c)){
+    c++;
   }
-  return count;
+  if(*c != '\0'){
+    return c;
+  }
+}
+
+char *word_end(char *str){
+  char *c = str;
+  while(space_char(*c)){
+    c++;
+  }
+  if(*c != '\0'){
+    return  c + 1;
+  }  
+}
+   
+int count_words(char *str){
 }
