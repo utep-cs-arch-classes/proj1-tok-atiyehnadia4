cb2dd2db76fbb43c289f1140ccbb2e33f3a12f4f
Nadia Atiyeh <natiyeh@miners.utep.edu>
2021-02-18 23:36:14 -0700

    modified gui.c to optimize retrival of User input and tested out word_start and word_end

--- src/gui.c
+++ src/gui.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-#include <tokenizer.h>
+#include "tokenizer.h"
 
 void userInputGUI(){
   char userInput[100];
   int i;
   char inputChar;
   printf("Please input a string\n");
-  while(inputChar != '\n'){
-    inputChar = getchar();
+  i = 0;
+  while((inputChar = getchar()) != '\n'){
     userInput[i] = inputChar;
     i = i+1;
+    putchar(inputChar);
   }
-  printf("%s\n", userInput);
+  userInput[i] = '\0';
+  printf("\n%c\n", *word_start(userInput));
+  printf("\n%c\n", *word_end(userInput));
+  
 }
 
 
