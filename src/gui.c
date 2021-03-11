203c65ae7f6f8f9e1f53b09028d22f49c047ff08
Nadia Atiyeh <natiyeh@miners.utep.edu>
2021-03-11 01:51:42 -0700

    Optomized the GUI to continue to ask foruser input and incorporated the history and tokenizer functions

--- src/gui.c
+++ src/gui.c
@@ -1,20 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
+#include "history.h"
 
-void userInputGUI(){
-  char userInput[100];
+
+char* getUserInput(){
+  char* userInput = malloc(100);
   int i;
   char inputChar;
-  printf("Please input a string\n");
-  i = 0;
+
   while((inputChar = getchar()) != '\n'){
     userInput[i] = inputChar;
-    i = i+1;
+    i = i + 1;
     putchar(inputChar);
   }
   userInput[i] = '\0';
-  print_tokens(tokenize(userInput));
-  printf("\n");
+
+  return userInput;
+}
+
+void getHistory(List* history){
+  char histChar;
+  char intKeyStr[3];
+  char exPoint;
+  int i;
+  
+  printf("\nWould you like to view your full history or individual history(F or I):\n");
+  histChar = getchar();
+  histChar = getchar();
+  
+  if(histChar == 'F'){
+    print_history(history);
+  }
+  if(histChar == 'I'){
+    printf("\nPlease enter the int key of history. (ex !1) \n");
+    exPoint = getchar();
+    i = 0;
+    while((exPoint = getchar()) != '\n'){
+      intKeyStr[i] = exPoint;
+      i = i + 1;
+    }
+    printf("%s\n", intKeyStr);
+    printf("\n%s\n", get_history(history,intKeyStr[1]- '0'));
+  }
+  
+}
+
+void userInputGUI(){
+  char *userInput;
+  char inputChar;
+  char **tokens;
+  List* history = init_history();
+  int cont = 1;
+  char conChar;
+
+  while(cont){
+    userInput = (char*)malloc(100);
+    printf("Please enter string\n");
+    userInput = copyStr(userInput, getUserInput());
+    
+    printf("\n");
+    tokens = tokenize(userInput);
+    print_tokens(tokens);
+    printf("\n");
+    add_history(history, userInput);
+    free_tokens(tokens);
+    printf("\n");
+    printf("\nContinue (Y or N)\n");
+    conChar = getchar();
+    if(conChar == 'Y'){
+      cont = 1;
+      userInput = copyStr(userInput, getUserInput());
+    }
+    else{
+      cont = 0;
+    }
+  }
+  
+  getHistory(history);
+  free_history(history);
 }
 
 
